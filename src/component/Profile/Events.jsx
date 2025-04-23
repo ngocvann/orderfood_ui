cb037a87d0aced4d9a81229fba8a2cf6a6e4bff2
Ngọc Vân <nguyenngocvan.qng@gmail.com>
2025-04-23 14:50:41 +0700

    profile

--- src/component/Profile/Events.jsx
+++ src/component/Profile/Events.jsx
@@ -3,8 +3,8 @@ import EventCard from './EventCard'
 
 const Events = () => {
   return (
-    <div className='mt-5 px-5 flex flex-wrap gap-5'>
-        {[1,1,1].map((item)=><EventCard/>)}
+    <div className='mt-5 pl-10 flex flex-wrap gap-5'>
+        {[1,1,1,1].map((item)=><EventCard/>)}
     </div>
   )
 }
